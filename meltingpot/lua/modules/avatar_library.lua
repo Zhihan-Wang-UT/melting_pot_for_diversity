1739c0ba9d66cd06516995b7a67d29b8e84af742
Zhihan Wang <zhihanwa@usc.edu>
2024-02-20 08:25:49 -0600

    added clean_up_mini substrate
    fix (instead of randomized) Initial Orientation facing North
    add clean_up_mini map

--- meltingpot/lua/modules/avatar_library.lua
+++ meltingpot/lua/modules/avatar_library.lua
@@ -95,7 +95,7 @@ function Avatar:__init__(kwargs)
       -- `randomizeInitialOrientation` (bool) default True. Avatar orientations
       -- are assigned randomly at the start of each episode. If you instead
       -- set this to false then initial Avatar orientation is always North.
-      {'randomizeInitialOrientation', args.default(true), args.booleanType},
+      {'randomizeInitialOrientation', args.default(false), args.booleanType},
   })
   Avatar.Base.__init__(self, kwargs)
   self._config.kwargs = kwargs
